5f77ddab6c87f94b14e58d4da05a70918df34226
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-27 18:02:15 -0400

    change task storage

--- script.js
+++ script.js
@@ -11,79 +11,67 @@ var currenthour = moment(now).format("HH");
  // var timeslotkeypi = parseInt(timeslotkeyls);
 var userupdatesarray = localStorage.getItem('newtask');
 var userupdatesarrayparsed = JSON.parse(userupdatesarray);
+
 console.log(userupdatesarrayparsed);
-console.log(userupdatesarray);
+//not sure why each function not working
+//$.each(userupdatesarrayparsed, function(usertimeslotKey,usertaskInput) {
+//$('textarea.background'+ userupdatesarrayparsed.usertimeslotKey +'PMrow').val(userupdatesarrayparsed.usertaskInput);
+//});
 
-//function placeuserudates () {
-// if (timelotsvalue = parseInt(usertimeslotKey.value)) {
-
-
-// }
-//}
-
-
+$(function()
+{
+for (let i=0; i < userupdatesarrayparsed.length; i++) {
+  $('textarea.background'+ userupdatesarrayparsed[i].usertimeslotKey +'PMrow').val(userupdatesarrayparsed[i].usertaskInput);
+};
+});
 
 // assign the current class to the hour in the class name
 var timeslots = [
   {
     timeslotclass: ".background8AMrow",
-    timeslotvalue: 8,
-    taskInput: "",
+    timeslotvalue: 8
   },
   {
     timeslotclass: ".background9AMrow",
-    timeslotvalue: 9,
-    taskInput: "",
+    timeslotvalue: 9
   },
   {
     timeslotclass: ".background10AMrow",
-    timeslotvalue: 10,
-    taskInput: "",
+    timeslotvalue: 10
   },
   {
     timeslotclass: ".background11AMrow",
-    timeslotvalue: 11,
-    taskInput: "",
+    timeslotvalue: 11
   },
   {
     timeslotclass: ".background12PMrow",
-    timeslotvalue: 12,
-    taskInput: "",
+    timeslotvalue: 12
   },
   {
     timeslotclass: ".background13PMrow",
-    timeslotvalue: 13,
-    taskInput: "",
+    timeslotvalue: 13
   },
   {
     timeslotclass: ".background14PMrow",
-    timeslotvalue: 14,
-    taskInput: "",
+    timeslotvalue: 14
   },
   {
     timeslotclass: ".background15PMrow",
-    timeslotvalue: 15,
-    taskInput: "",
+    timeslotvalue: 15
   },
   {
     timeslotclass: ".background16PMrow",
-    timeslotvalue: 16,
-    taskInput: "",
+    timeslotvalue: 16
   },
   {
     timeslotclass: ".background17PMrow",
-    timeslotvalue: 17,
-    taskInput: "",
+    timeslotvalue: 17
   },
   {
     timeslotclass: ".background18PMrow",
-    timeslotvalue: 18,
-    taskInput: "",
+    timeslotvalue: 18
   },
 ];
-//
-  
-// 
 
 //function activitybackgroundcolor() {
 
@@ -111,35 +99,22 @@ for (let i = 0; i < timeslots.length; i++) {
 //add a new task
 //recognize save button click
 //creates a key and value to send to storage
-
-$(".saveBtn").on("click", function () {
+$(function()
+{
+  var userrevisedtasksarray=[];
+$(".saveBtn").on("click", function ()
+ {
   var savedtaskInput = $(this).siblings("textarea").val().trim();
   var timeslotkey = $(this).siblings()[1].className.slice(17, 19);
   var userrevisedtasks = { 
     usertimeslotKey: timeslotkey,
     usertaskInput: savedtaskInput,
   };
-
-  window.localStorage.setItem('newtask',JSON.stringify(userrevisedtasks));
+  userrevisedtasksarray.push(userrevisedtasks);
+  console.log(userrevisedtasksarray);
+  window.localStorage.setItem('newtask',JSON.stringify(userrevisedtasksarray));
 });
-  
-  //localStorage.setItem(timeslotkeyint,savedtaskInput);
+});
 
- // var timeslotkeyls = localStorage.getItem(key);
- // var timeslotkeypi = parseInt(timeslotkeyls);
- // var savedtaskInputls = localStorage.getItem(value);
- // console.log(timeslotkeypi);
- // console.log(savedtaskInputls);
-
-//push the local storage into the vartimeslots array
-//check for match of key item and identifier in array
-//push item into array
-// to clean out localStorage.clear();
-//if (timeslotkeyint === timeslots[4].timeslotvalue) {
-//  (timeslots[4].taskInput = savedtaskInput);
-//  console.log("criteriamet");
-//  console.log(timeslots[4].taskInput);
-// } else {
-// console.log("criteria Failed")};
-
-//push to innerHTML to appear upon refresh
+//not sure why changes from the time before not saved
+// Note: to clean out localStorage.clear();
